c25f8fe4756e102518d6418812ba350edb7ff97b
kjellski <otto.kjell@gmail.com>
2012-12-22 15:33:47 +0100

    added backup and linking, relpath not working though

--- lib/DotFile.py
+++ lib/DotFile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os.path, datetime, logging
+import os.path, datetime, logging, shutil, sys
 
 # this class represents a file of an profile
 # its able to create a backup of the original file
@@ -8,7 +8,7 @@ import os.path, datetime, logging
 class DotFile: 
 
     # where this file lies arround
-    working_path = os.path.join('tmp', 'test')
+    profile_path = os.path.join('tmp', 'test')
     # where this file should be deployed
     target_path = os.path.join('tmp', 'target')
     # if existing, where should the target file be copied to
@@ -16,24 +16,61 @@ class DotFile:
         'backups',
          datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
 
+    # if file is not starting with ., add that
     def __init__(self, filename):
-        self.filename = filename
+        if filename.startswith('.'):
+            self.filename = filename
+        else:
+            self.filename = '.' + filename
 
-        print self
+        self.target_file_path = os.path.join(DotFile.target_path, self.filename)
+        self.backup_file_path = os.path.join(DotFile.backup_path, self.filename)
+        self.profiles_file_path = os.path.join(DotFile.profile_path, filename)
+
+        logging.debug(self)
 
     # creates a link from the home folder to this file
-    def link():
+    def link(self):
         # check for existence, backup
-        logging.info('link')
+        self.backup()
+        #link
+        logging.info('linking from ' + self.profiles_file_path + \
+            ' to ' + \
+            self.target_file_path)
+
+        os.symlink(self.profiles_file_path, os.path.relpath(self.profiles_file_path, self.target_file_path))
 
     # creates a backup for the file 
-    def backup():
+    def backup(self):
         # does it exist? 
-        logging.info('backup')
-        #copy over to backup folder
+        if os.path.exists(self.target_file_path):
+            try:
+                logging.info('backing up ' + self.target_file_path)
+                if os.path.isdir(self.target_file_path):
+                    # copy whole tree over to backup folder
+                    shutil.copytree(self.target_file_path, DotFile.backup_path)
+                    logging.info('now deleting the backed up directory: ' + self.target_file_path)
+                    # if the folder is a link, remove it like a file... 
+                    if os.path.islink(self.target_file_path):
+                        os.remove(self.target_file_path)
+                    else:
+                        shutil.rmtree(self.target_file_path)
+
+                else:
+                    # copy the file 
+                    shutil.copyfile(self.target_file_path, self.backup_file_path)
+                    logging.info('now deleting the backed up file: ' + self.target_file_path)
+                    os.remove(self.target_file_path)
+
+            except Exception, e:
+                logging.error(e)
+                logging.error('while backing up, something failed, EXITING for data safety.')
+                sys.exit(-1)
 
     def __str__(self):
-        return 'DotFile(working_path=' + DotFile.working_path + ', ' + \
-            'target_path=' + DotFile.target_path + ', ' + \
-            'backup_path=' + DotFile.backup_path + ', ' + \
-            'self.filename=' + self.filename + ')'
+        return 'DotFile(' + \
+                '\n\tself.profiles_file_path=' + self.profiles_file_path + ', ' + \
+                '\n\tself.target_file_path=' + self.target_file_path + ', ' + \
+                '\n\tself.backup_file_path=' + self.backup_file_path + ', ' + \
+                '\n\tself.filename=' + self.filename + \
+            ')'
